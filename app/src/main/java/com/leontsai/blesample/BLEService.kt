d28ddd6da368db5d89998da24170f2ef4087b5c5
caiyunlong <caiyunlong2009@gmail.com>
2019-01-25 17:42:49 +0800

    UI

--- app/src/main/java/com/leontsai/blesample/BLEService.kt
+++ app/src/main/java/com/leontsai/blesample/BLEService.kt
@@ -10,16 +10,27 @@ import android.content.Intent
 import android.os.Binder
 import android.os.Handler
 import android.os.IBinder
-import android.os.Message
+import java.util.*
 
 class BLEService : Service() {
+
+    companion object {
+        val FOUND_DEVICE = 802
+        val DISCOVERY = 803
+        val WRITE = 804
+        val RECEIVE_DEVICE_MSG = 805
+        val DESCRIPTION_WRITE = 806
+    }
+
+
     private val mBinder = LocalBinder()
-    private lateinit var mBluetoothManager: BluetoothManager
-    private lateinit var mBluetoothAdapter: BluetoothAdapter
-    private lateinit var mBluetoothLeScanner: BluetoothLeScanner
-    private lateinit var mScanCallback: ScanCallback
-    private lateinit var mBluetoothGatt: BluetoothGatt
-    private lateinit var mBluetoothDevice: BluetoothDevice
+    private var mBluetoothManager: BluetoothManager? = null
+    private var mBluetoothAdapter: BluetoothAdapter? = null
+    private var mBluetoothLeScanner: BluetoothLeScanner? = null
+    private var mScanCallback: ScanCallback? = null
+    private var mBluetoothGatt: BluetoothGatt? = null
+    private var mBluetoothDevice: BluetoothDevice? = null
+    @Volatile
     private var mBluetoothDeviceFound = false
 
     private val mBluetoothGattCallback = object : BluetoothGattCallback() {
@@ -49,9 +60,23 @@ class BLEService : Service() {
 
     }
 
-    private val mHandler=Handler{
-        when(it.what){
+    private val mHandler: Handler by lazy {
+        Handler {
+            when (it.what) {
+                FOUND_DEVICE -> {
+                    scanDevice(false)
+                    mBluetoothDeviceFound = true
+                    mHandler.postDelayed(Runnable { connect() }, 200)
+                }
+                DISCOVERY -> {
 
+                }
+
+                else -> {
+
+                }
+            }
+            return@Handler true
         }
     }
 
@@ -61,10 +86,11 @@ class BLEService : Service() {
             mBluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
         }
         if (mBluetoothManager == null) return false
+
         if (mBluetoothAdapter == null)
-            mBluetoothAdapter = mBluetoothManager.adapter
+            mBluetoothAdapter = mBluetoothManager?.adapter
         if (mBluetoothAdapter == null) return false
-        if (mBluetoothLeScanner == null) mBluetoothLeScanner = mBluetoothAdapter.bluetoothLeScanner
+        if (mBluetoothLeScanner == null) mBluetoothLeScanner = mBluetoothAdapter?.bluetoothLeScanner
         if (mBluetoothLeScanner == null) return false
         if (mScanCallback == null)
             mScanCallback = object : ScanCallback() {
@@ -76,8 +102,8 @@ class BLEService : Service() {
                     }
 
                     val device = result.device
-                    if (device.name.startsWith(GATTATTRIBUTES_DEVICE_NAME)){
-                        mBluetoothDevice=device
+                    if (device.name.startsWith(GATTATTRIBUTES_DEVICE_NAME)) {
+                        mBluetoothDevice = device
                     }
                 }
 
@@ -85,20 +111,60 @@ class BLEService : Service() {
                     super.onScanFailed(errorCode)
                 }
             }
-        return mBluetoothAdapter.isEnabled
+        return mBluetoothAdapter!!.isEnabled
     }
 
     fun scanDevice(enable: Boolean) {
         if (enable) {
-            mBluetoothLeScanner.startScan(mScanCallback)
+            mBluetoothLeScanner?.startScan(mScanCallback)
         } else {
-            mBluetoothLeScanner.stopScan(mScanCallback)
+            mBluetoothLeScanner?.stopScan(mScanCallback)
         }
     }
 
-    fun connect() {
-
+    fun connect(): Boolean {
+        if (mBluetoothDevice == null) return false
+        mBluetoothGatt = mBluetoothDevice?.connectGatt(this, false, mBluetoothGattCallback)
+        if (mBluetoothGatt == null) return false
+        return true
     }
+
+    private fun setCharacteristicNotification(serviceUUID: UUID, characteristicUUID: UUID): Boolean {
+        val bluetoothGattService = mBluetoothGatt?.getService(serviceUUID)
+
+        if (bluetoothGattService == null) return false
+
+        val bluetoothGattCharacteristic = bluetoothGattService.getCharacteristic(characteristicUUID)
+
+        if (bluetoothGattCharacteristic == null) return false
+
+        val descriptors = bluetoothGattCharacteristic.descriptors
+
+        if (descriptors == null) return false
+
+        var result = mBluetoothGatt?.setCharacteristicNotification(bluetoothGattCharacteristic, true) ?: false
+
+        descriptors.forEach {
+            it.value = BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE
+            result = result && (mBluetoothGatt?.writeDescriptor(it) ?: false)
+        }
+
+        return result
+    }
+
+    private fun writeCharacteristic(serviceUUID: UUID, characteristicUUID: UUID, value: ByteArray): Boolean {
+        val bluetoothGattService = mBluetoothGatt?.getService(serviceUUID)
+
+        if (bluetoothGattService == null) return false
+
+        val bluetoothGattCharacteristic = bluetoothGattService.getCharacteristic(characteristicUUID)
+
+        if (bluetoothGattCharacteristic == null) return false
+
+        bluetoothGattCharacteristic.value = value
+        return mBluetoothGatt?.writeCharacteristic(bluetoothGattCharacteristic) ?: false
+    }
+
 
     override fun onBind(intent: Intent?): IBinder? {
         return mBinder
@@ -111,5 +177,9 @@ class BLEService : Service() {
         }
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        mHandler.removeCallbacksAndMessages(null)
+    }
 
 }
